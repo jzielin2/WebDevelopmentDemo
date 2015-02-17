7d4d98a105d220bd43c7758699cb2e1037617b41
John Zielinski <jozielinski@deloitte.com>
2015-02-16 22:37:08 -0500

    Updated user lookup file

--- form.php
+++ form.php
@@ -12,7 +12,7 @@
 	</div>
 
 	<div class="container-account-creation-form">
-		<form action="form.php" method="post">
+		<form action="form.php" method="get">
 			<input type="text" name="email" placeholder="Enter your Email" /> <br/>
 			<input type="Password" name="password" placeholder="Enter your Password" /> <br/>
 			<input type="text" name="name" placeholder="Enter your name" /> <br/>
@@ -23,24 +23,41 @@
 	<div class="container-results">
 		<?php
 
-			if(!isset($_POST["email"]) || !isset($_POST["password"])){}
-			else{
-				$email = $_POST["email"];
-				$password = $_POST["password"];
-				$name = $_POST["name"];
-				$conn = dbConnect();
+			if(isset($_GET["email"]) && isset($_GET["password"])){
 
-				$insertUser = "INSERT INTO userAccountsPlain (email, password, name) VALUES ('$email', '$password', '$name')";
+				//Save all the data from the form into three variables we can use here
+				$email = $_GET["email"];
+				$password = $_GET["password"];
+				$name = $_GET["name"];
 				
-				if(mysqli_query($conn, $insertUser)){
-					echo "Great - you are now registered with the email " . $email . ". <a href=\"userLookup.php\">Now look up your User ID</a>";
-				}
-				else{
-					echo "Error: " . mysqli_error($conn);
-				}
+				//Write our SQL statement using the three variables we just created from the form.
+				//Store this query in a new variable we can use to pass to the database.
+				$insertUserQuery = "INSERT INTO userAccountsPlain (email, password, name) 
+														VALUES ('$email', '$password', '$name')";
+
+				//Open a new connection to the database server so we can pass our query to it. 
+				//Store the connection information in a variable we can use to tell our script 
+				//how to connect to the DB
+				$DBConnectionDetails = dbConnect();
+				
+				//Use our DB connection information to pass our query to the database.  We won't 
+				//worry about storing any response since we're not looking to get back any 
+				//information in this query
+				mysqli_query($DBConnectionDetails, $insertUserQuery);
+
+				//Use the "echo" function to output a success message to the user.
+				echo "Great - you are now registered with the email " . $email . ". ";
+				echo "<a href=\"userLookup.php\">Now look up your User ID</a>";
+
+			}
+			else{
+			
 			}
 
 		?>
+
+		
+
 	</div>
 </div>
 
--- userLookup.php
+++ userLookup.php
@@ -12,7 +12,7 @@
 
 	<div class="container-account-creation-form">
 		<p>Enter your email below and we'll let you know the name currently linked to your account</p>
-		<form action="userLookup.php" method="post">
+		<form action="userLookup.php" method="get">
 			<input type="text" name="email" placeholder="Enter your Email" /> <br/>
 			<input type="Submit" />
 		</form>
@@ -21,30 +21,45 @@
 	<div class="container-results">
 		<?php
 
-			if(!isset($_POST["email"])){}
-			else{
-				$email = $_POST["email"];
-				$conn = dbConnect();
-
+				
 				//Inject string that illusetrates pulling back more data: 	test' OR 'one'='one
-				//test' UNION ALL select CONCAT(email, ' -> ', password) as name FROM userAccountsPlain WHERE '1' = '1
+				//whatever' UNION ALL SELECT CONCAT(email, ' -> ', password) AS name FROM userAccountsPlain WHERE 'one' = 'one
+
+			if(isset($_GET["email"])){
 				
-				$getUserId = "SELECT name FROM userAccountsPlain WHERE email = '$email'";
-				$results = mysqli_query($conn, $getUserId);
+				//Save the data from the form into a variable we can use here
+				$email = $_GET["email"];
 				
-				if($results){
-					echo "The name registered for this account is ";
-					
-					$row = mysqli_fetch_assoc($results);
-					echo $row['name'];
-					while ($row = mysqli_fetch_assoc($results))
-					{
-						echo "<br/>" . $row['name'];
-					}
+				//Write our SQL statement using the three variables we just created from the form.
+				//Store this query in a new variable we can use to pass to the database.				
+				$getNameQuery = "SELECT name FROM userAccountsPlain WHERE email = '$email'";
+
+				//Open a new connection to the database server so we can pass our query to it. 
+				//Store the connection information in a variable we can use to tell our script 
+				//how to connect to the DB				
+				$DBConnectionDetails = dbConnect();
+
+				//Use our DB connection information to pass our query to the database.  We then
+				//store the results in a new variable, $queryResults
+				$queryResults = mysqli_query($DBConnectionDetails, $getNameQuery);
+				
+				//Write the stock part of our message to the screen
+				echo "The name registered for this account is ";
+
+				//We now need to write the results of our query to show the names that were found.
+				//The details aren't important here. At a high level, we can only deal with results
+				//one row at a time.  We load the first name returned by the query and write it on 
+				//the screen.  We then check for a second name. If it exists, we write it to the
+				//the screen and check for a third.  If it doesn't exist we stop.
+				$row = mysqli_fetch_assoc($queryResults);
+				echo $row['name'];
+				while ($row = mysqli_fetch_assoc($queryResults)){
+					echo "<br/>" . $row['name'];
 				}
-				else{
-					echo "Error: " . mysqli_error($conn);
-				}
+			
+			}
+			else{
+				
 			}
 
 		?>
